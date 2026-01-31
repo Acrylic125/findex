8e54901edd983fa35ae22e212d853acbce5a849b
Acrylic125 <acrylic125email@gmail.com>
2026-01-31 11:21:25 +0800

    chore: setup

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,10 +1,37 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { db } from "@/db";
+import { programsTable } from "@/db/schema";
 
-export default function Page() {
+// https://www.ntu.edu.sg/education/colleges-schools
+const schools = [
+  "NBS",
+  "CCDS",
+  "CCEB",
+  "EEE",
+  "CEE",
+  "MSE",
+  "MAE",
+  "ADM",
+  "SCH",
+  "SSS",
+  "WKWSCI",
+  "LCKM",
+  "SPMS",
+  "SBS",
+  "ASE",
+  "NIE",
+];
+
+export default async function Page() {
+  // const programs = await db.select().from(programsTable);
+
   return (
     <main>
       <ScrollArea className="bg-background text-foreground h-screen">
         <h1>Hello World</h1>
+        {schools.map((school) => (
+          <div key={school}>{school}</div>
+        ))}
       </ScrollArea>
     </main>
   );
--- ui/src/db/index.ts
+++ ui/src/db/index.ts
@@ -2,5 +2,5 @@ import { env } from "@/lib/env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-export const client = postgres(env.DATABASE_URL, { prepare: false });
-export const db = drizzle(client);
+const queryClient = postgres(env.DATABASE_URL);
+export const db = drizzle({ client: queryClient });
