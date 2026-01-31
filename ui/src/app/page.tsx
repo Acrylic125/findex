3b0d2499757090c1ccb1a5356160cb109e32a3a2
Acrylic125 <acrylic125email@gmail.com>
2026-01-31 10:04:44 +0800

    chore: remove template files

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -3,17 +3,10 @@
 import { Section, Cell, Image, List } from "@telegram-apps/telegram-ui";
 import { useTranslations } from "next-intl";
 
-import { Link } from "@/components/Link/Link";
-import { LocaleSwitcher } from "@/components/LocaleSwitcher/LocaleSwitcher";
-import { Page } from "@/components/Page";
-
-import tonSvg from "./_assets/ton.svg";
 import { useRawInitData } from "@tma.js/sdk-react";
-import { useQuery } from "@tanstack/react-query";
 import { trpc } from "@/server/client";
 
 export default function Home() {
-  const rawInitData = useRawInitData();
   // const res = useQuery({
   //   queryKey: ["rawInitData"],
   //   queryFn: () => {
